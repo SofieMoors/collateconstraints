c8d86094a5a5cca907999e125029407d87d41225
Sofie Moors <SMoors@ad.ua.ac.be>
2022-05-13 09:50:23 +0200

    collation notebook

--- src/refactor_special_token (1)/refactor_special_token.py
+++ src/refactor_special_token (1)/refactor_special_token.py
@@ -2,27 +2,20 @@
 1 maak een map 'correct_unicodes'
 2 vul het lijstje met replacements verder aan met de juiste codes
 3 verander onderaan de directories zodat ze naar het juiste verwijzen
---> als je een foutmelding krijgt 'KeyError: 'x' ' dan is het die letter x die mist in replacements en moet je daarvoor
-     nog de code zoeken en toevoegen
 """
 import os, codecs
 
 
 def make_list(directory, output_path_list, output_path_file):
     replacements = {'a': u'\u0101',
-                    'A': u'\u0100',
                     'e': u'\u0113',
-                    'E': u'\u0112',
-                    'n': u'\uE5DC',
-                    'N': u'\uE1DC',
+                    'n': 'nn',
                     'o': u'\u014D',
+                    'u': 'û',
+                    'i': 'î',
+                    'm': '^m',
                     'O': u'\u014C',
-                    'u': u'\016B',
-                    'U': u'\016A',
-                    'i': u'\012B',
-                    'I': u'\012A', 
-                    'm': u'\E5B8',
-                    'M': u'\E1B8'}  # <-- voeg hier de streepjes toe op de 2 letter (ik heb voorlopig hoedjes gezet)
+                    't': '^t'}  # <-- voeg hier de streepjes toe op de 2 letter (ik heb voorlopig hoedjes gezet)
                                 # altijd 1 \ en dan een kleine u en geen plus en zet een u voor het '
     words_with_special_token = [] # start een lege lijst om de woorden met speciale tekens voorlopig in te zetten
     output_file = [] # start een lege lijst om de output lijnen voorlopig in te zetten
@@ -49,20 +42,14 @@ def make_list(directory, output_path_list, output_path_file):
                 output_file.append(new_line) #voeg de nieuwe lijn toe aan de output
         with codecs.open(os.path.join(output_path_file, file), 'w', encoding='utf8') as output_f: # we maken een nieuw bestand met dezelfde naam (bvb A.txt) in de nieuwe folder (bvb correcte_unicode)
             for line in output_file:
-                output_f.write(line.strip() + '\n') # schrijf de output naar een bestand
-                
+                output_f.write(line) # schrijf de output naar een bestand
     with open(output_path_list, 'w', encoding='utf-8') as output_l: #open een bestand om de lijst op te slaan
         for i in words_with_special_token:
-            output_l.write(i.strip() + '\n') # sla de lijst op met op elke regel 1 woord
+            output_l.write(i + '\n') # sla de lijst op met op elke regel 1 woord
 
 
 if __name__ == '__main__':
-    input_directory = r'C:\Users\SMoors\Gitrepos\collatecontraints\processed_txt_martijn'
-    # dit is de map die alle originele txt's bevat (bvb A.txt)
-    output_path_list = r'C:\Users\SMoors\Gitrepos\collatecontraints\correct_unicodes\words_with_special_token.txt'
-    # dit is een bestand waarin alle woorden met een speciaal teken naartoe geschreven worden,
-    # op zich heb je dat niet nodig, maar misschien kan dat in de toekomst handig zijn (bvb om mooi te printen)
-    output_path_file = r'C:\Users\SMoors\Gitrepos\collateconstraints\correct_unicodes'
-    # dit is een nieuwe map waarin een nieuwe versie van elk txt bestand zal worden ingezet.
-    # In die bestanden zijn de karakters met streep de juiste code
+    input_directory = r'C:\Users\u0144004\OneDrive - KU Leuven\PhD\sofie'
+    output_path_list = r'C:\Users\u0144004\OneDrive - KU Leuven\PhD\words_with_special_token.txt'
+    output_path_file = r'C:\Users\u0144004\OneDrive - KU Leuven\PhD\correct_unicodes'
     make_list(input_directory, output_path_list, output_path_file)
